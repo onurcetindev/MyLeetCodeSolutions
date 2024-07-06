274e21136c4072eac150601397577cd0b8a1bef9
Onur Cetin <onucetin003@gmail.com>
2024-07-06 11:28:24 +0300

    Divide Two Integers Problem is Solved

---
+++ Medium/29. Divide Two Integers/DivideTwoIntegers.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int divide(int dividend, int divisor) {
+        
+        int result = dividend / divisor; 
+       
+
+        if (dividend == Integer.MIN_VALUE && divisor == -1) {
+            return Integer.MAX_VALUE;
+        }
+        if (dividend == Integer.MIN_VALUE && divisor == 1) {
+            return Integer.MIN_VALUE;
+        }
+
+        return result;
+        
+        
+    }
+}
