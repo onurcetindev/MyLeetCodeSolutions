52faaee59e2482c9e50d187c3f585467b34f37f4
Onur Cetin <onucetin003@gmail.com>
2024-07-06 19:48:11 +0300

    Reverse String Problem is Solved

---
+++ Easy/344. Reverse String/ReverseString.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void reverseString(char[] s) {
+
+     int left = 0, right = s.length - 1;
+        while (left < right) {
+            // Swap the characters
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
+             
+    }
+  }
+}
