fa406476dbe03bee4c419e0aa6c1f8bc5d12d778
Onur Cetin <onucetin003@gmail.com>
2024-07-12 22:39:30 +0300

    Maximum Binary Tree Problem is Solved

---
+++ Medium/654. Maximum Binary Tree/MaximumBinaryTree.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode constructMaximumBinaryTree(int[] nums) {
+        return buildTree(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode buildTree(int[] nums, int left, int right) {
+        if (left > right) {
+            return null;
+        }
+
+        // Find the index of the maximum value in the current subarray
+        int maxIndex = left;
+        for (int i = left + 1; i <= right; i++) {
+            if (nums[i] > nums[maxIndex]) {
+                maxIndex = i;
+            }
+        }
+
+        // Create the root node with the maximum value
+        TreeNode root = new TreeNode(nums[maxIndex]);
+
+        // Recursively build the left and right subtrees
+        root.left = buildTree(nums, left, maxIndex - 1);
+        root.right = buildTree(nums, maxIndex + 1, right);
+
+        return root;
+    }
+}
